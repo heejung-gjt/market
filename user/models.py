fd012e59e502f390537c7ec8e74ebe6b18e2308b
heejung-gjt <hee_9803@naver.com>
2021-06-09 20:05:50 +0900

    feat: add user model
    - create profile model
    
    close #4

--- user/models.py
+++ user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
-
+from behaviors import Nameable, TimeStampable, Deleteable
 # Create your models here.
+
+class User(TimeStampable, Deleteable):
+  username = models.CharField(max_length=64)
+  email = models.EmailField(max_length=64)
+  password = models.CharField(max_length=64)
+
+  def __str__(self):
+    return self.username
+  
+  class Meta:
+    db_table = 'users'
+
+
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+  nickname = models.CharField(max_length=64)
