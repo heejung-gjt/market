5337fadfb19b716fd3c4cd47d965ecd4b9e32b52
heejung-gjt <hee_9803@naver.com>
2021-09-13 20:53:42 +0900

    docs: add env file

--- market/settings.py
+++ market/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c3@6p^-x=(*a7lpr$lu7-b-4bg*^jf=6pw4^dz(afvu#p+o32t'
+SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
